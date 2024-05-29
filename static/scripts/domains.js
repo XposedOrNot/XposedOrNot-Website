bdfde6579b38ed08eace9da7800b06eb9d544c08
DevaOnBreaches <devaonbreaches@gmail.com>
2024-05-29 18:13:54 +0530

    ♻️ refactor: Updated domain breach summary with new version, removed old

--- static/scripts/domains.js
+++ static/scripts/domains.js
@@ -1,102 +1,113 @@
-function val_e(input) {
-    var reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
-    if (reg.test(input)) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
 $(document).ready(function () {
-    $('#defaultForm')
-        .bootstrapValidator({
-            feedbackIcons: {
-                valid: 'glyphicon glyphicon-ok',
-                invalid: 'glyphicon glyphicon-remove',
-                validating: 'glyphicon glyphicon-refresh'
-            },
-            live: 'enabled',
-            fields: {
-                'name': {
-                    validators: {
-                        regexp: {
-                            regexp: /^(?!:\/\/)([a-zA-Z0-9]+\.)+[a-zA-Z]{2,}$/i,
-                            message: ' Please enter a valid domain address ...'
-                        },
-                        notEmpty: {
-                            message: ' Please enter a valid domain address ...'
+    $('#domainModal').modal('show');
+    $('.overlay').show();
 
-                        }
+    const domainRegex = /^(?!:\/\/)([a-zA-Z0-9-_]+\.)?[a-zA-Z0-9][a-zA-Z0-9-_]+\.[a-zA-Z]{2,11}?$/;
+
+    $('#domainForm').on('submit', function (e) {
+        e.preventDefault();
+        var domainName = $('#domainInput').val();
+
+        if (domainRegex.test(domainName)) {
+            $('#submitButton').attr("disabled", true);
+            $('#spinner').show();
+
+            $('#domain-display').text(domainName);
+            var logoSrc = "https://logo.clearbit.com/" + domainName;
+            $("#logo").attr("src", logoSrc).show();
+
+            var breachApiUrl = 'https://api.xposedornot.com/v1/domain-breach-summary?d=' + domainName;
+            $.ajax(breachApiUrl)
+                .done(function (data) {
+                    var breaches = data.sendDomains.breaches_details;
+                    var totalBreaches = 0;
+                    var totalRecords = 0;
+                    var totalEmails = 0;
+
+                    breaches.forEach(function (breach) {
+                        totalBreaches++;
+                        totalRecords += parseInt(breach.breach_pastes);
+                        totalEmails += parseInt(breach.breach_emails);
+                    });
+
+                    $('#breach-count').text(totalBreaches);
+                    $('#record-count').text(totalRecords >= 50 ? "50+" : totalRecords);
+                    $('#email-count').text(totalEmails >= 50 ? "50+" : totalEmails);
+
+                    var lastExposure = breaches.length > 0 ? breaches[0].last_exposure : "--";
+                    $('#last-exposure').text(lastExposure);
+
+                    $('#domainModal').modal('hide');
+                    $('.overlay').hide();
+                    $('#content').removeClass('blurred');
+                })
+                .fail(function (jqXHR) {
+                    if (jqXHR.status === 404) {
+                        $('#searchedDomain').text(domainName);
+                        $('#domainForm').hide();
+                        $('#domainModalLabel').hide();
+                        $('#noBreachMessage').show();
+                        triggerConfetti();
+                    } else if (jqXHR.status === 429) {
+                        var retryAfter = JSON.parse(jqXHR.responseText).retry_after_seconds;
+                        $('#errorMessage').text(`Rate limit exceeded. Please try again after ${retryAfter} seconds.`).show();
+                    } else {
+                        $("#alertMe").text("An error occurred. Please try again later.");
                     }
-                },
-            },
-            onSuccess: function (e, data) {
-                $("#alertMe").html(`Loading ... <span class="fa fa-spinner fa-spin" role="status" aria-hidden="true"></span> `);
-                $('#t').html('-');
-                $('#b').html('-');
-                $('#p').html('-');
-                $("#succ").hide();
-                edutuc1 = 'https://api.xposedornot.com/v1/domain-breach-summary?d=' + $('#eventName').val()
-                dom = $('#eventName').val()
-                var myjsonc1;
-                var jc1 = $.ajax(edutuc1)
-                    .done(function (nc1) {
-                        e.preventDefault();
-                        jd2 = nc1;
-                        msg = jd2.SearchStatus;
-                        e = p = t = 0
-                        for (var i = 0; i < jd2.sendDomains.breaches_details.length; i++) {
-                            id_cnt = i + 1;
-                            e = jd2.sendDomains.breaches_details[i].breach_emails;
-                            p = jd2.sendDomains.breaches_details[i].breach_pastes;
-                            t = parseInt(e) + parseInt(p);
-                            $('#t').html(t);
-                            $('#b').html(e);
-                            $('#p').html(p);
-                            if (t == 0) {
-                                $("#succ").show();
-                                $("#dang").hide();
-                            }
-                            if (e >= 50) {
-                                $("#bx").css("background-color", "indianred");
-                                $('#b').html("50+")
-                                $("#dang").show();
-                                $("#succ").hide();
-                            } else {
-                                $('#b').html(e);
-                            }
-                            if (p >= 50) {
-                                $('#p').html("50+")
-                                $("#al").show();
-                                $("#su").hide();
-                            } else {
-                                $('#p').html(p);
-                            }
-                            $("#getBreach").show();
-                            logo_src = "//logo.clearbit.com/" + dom
-                            $("#logo").attr("src", logo_src);
-                            $("#logo").show();
-                            $("#dom").show();
-                        }
-                        $("#alertMe").text("Get Me Breach Data");
-                    })
-                    .fail(function (nc1) {
-                        if (nc1.status === 404) {
-                            $("#alertMe").text("No breach data found for this domain.");
-                            $("#dang").show();
-                            $("#succ").hide();
-                        } else {
-                            $("#alertMe").text("An error occurred. Please try again later.");
-                            $("#dang").show();
-                            $("#succ").hide();
-                        }
-                        $('#t').html('-');
-                        $('#b').html('-');
-                        $('#p').html('-');
-                        $("#getBreach").hide();
-                        return false
-                    })
-                e.preventDefault();
+                    $('#breach-count').text('-');
+                    $('#record-count').text('-');
+                    $('#email-count').text('-');
+                    $('#last-exposure').text('--');
+                })
+                .always(function () {
+                    $('#submitButton').attr("disabled", false);
+                    $('#spinner').hide();
+                });
+        } else {
+            $('#domainInput').addClass('is-invalid');
+        }
+    });
+
+    $('#checkAnotherDomain').on('click', function () {
+        $('#noBreachMessage').hide();
+        $('#domainForm').show();
+        $('#domainModalLabel').show();
+        $('#domainInput').val('').removeClass('is-invalid');
+        $('#domainModal').modal('show');
+        $('.overlay').show();
+    });
+
+    $('#checkAnotherDomainBtn').on('click', function () {
+        $('#domainModal').modal('show');
+        $('.overlay').show();
+    });
+
+    $('#domainInput').on('input', function () {
+        $(this).removeClass('is-invalid');
+        $('#errorMessage').hide();
+    });
+
+    function triggerConfetti() {
+        var end = Date.now() + 5 * 1000;
+
+        (function frame() {
+            confetti({
+                particleCount: 5,
+                angle: 60,
+                spread: 55,
+                origin: { x: 0 }
+            });
+            confetti({
+                particleCount: 5,
+                angle: 120,
+                spread: 55,
+                origin: { x: 1 }
+            });
+
+            if (Date.now() < end) {
+                requestAnimationFrame(frame);
             }
-        });
+        }());
+    }
 });
+
