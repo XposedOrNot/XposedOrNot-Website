e59f3d8b261d892b0acd977aeded3351494059e0
DevaOnBreaches <devaonbreaches@gmail.com>
2025-03-31 09:05:26 +0530

    📦 assets: Added new breach logo

--- static/scripts/other-libraries.js
+++ static/scripts/other-libraries.js
@@ -1,4 +1,4 @@
-(function (h, o, t, j, a, r) {
+/*(function (h, o, t, j, a, r) {
     h.hj = h.hj || function () {
         (h.hj.q = h.hj.q || []).push(arguments)
     };
@@ -12,6 +12,7 @@
     r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
     a.appendChild(r);
 })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+*/
 /*
 (function() {
     var script = document.createElement("script");
